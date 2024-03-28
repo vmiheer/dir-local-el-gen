f326f93caeb867d22569cac2a5654833a7cdb471
Miheer Vaidya <m.vaidya@utah.edu>
2024-03-28 01:44:54 -0600

    fix clippy warning

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,6 @@ fn main() {
     let pypath = std::env::var("PYTHONPATH");
     let pypath_vec = pypath
         .as_ref()
-        .map_or(Vec::new(), |v| v.split(":").collect::<Vec<&str>>());
+        .map_or(Vec::new(), |v| v.split(':').collect::<Vec<&str>>());
     println!("PYTHONPATH: {}", to_string(&pypath_vec).unwrap_or_default());
 }
